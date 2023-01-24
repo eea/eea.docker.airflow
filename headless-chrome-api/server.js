b2b66a52eb4a8dfe3407cd9ba1e224297ce7bda3
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-01-24 15:04:28 +0200

    make linter happy

--- headless-chrome-api/server.js
+++ headless-chrome-api/server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const puppeteer = require('puppeteer')
+const express = require('express');
+const app = express();
+const port = 3000;
+const puppeteer = require('puppeteer');
 
-const fs = require('fs')
-const fp = require('path')
-const screenshotDir = '/tmp/screenshot/'
-const pdfDir = '/tmp/pdf/'
-const hbs = require('express-hbs')
-const mustache = require('mustache')
+const fs = require('fs');
+const fp = require('path');
+const screenshotDir = '/tmp/screenshot/';
+const pdfDir = '/tmp/pdf/';
+const hbs = require('express-hbs');
+const mustache = require('mustache');
 
 // request header
 // User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0
-const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0'
+const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0';
 // need to add this header for some request
 // Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
 // const accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
@@ -37,11 +37,11 @@ let browser
     fs.mkdir(screenshotDir, { recursive: true }, (err) => {
         if (err) throw err;
     });
-    console.log("screenshot dir created")
+    console.log("screenshot dir created");
     fs.mkdir(pdfDir, { recursive: true }, (err) => {
         if (err) throw err;
     });
-    console.log("pdf dir created")
+    console.log("pdf dir created");
     
 })();
 
@@ -58,86 +58,87 @@ app.set('views', __dirname + '/views');
 app.use(express.static(relative('public')));
 
 app.get('/', (req, res) => {
-    res.send('headless chrome api is ready to serve')
+    res.send('headless chrome api is ready to serve');
 }) 
 
 app.get('/generate/:template', (req, res) => {
-    const template = req.params.template
+    const template = req.params.template;
     const data = req.query;
-    console.log(data)
+    console.log(data);
     try {
         var content = fs.readFileSync(relative('templates/' + template), 'utf8');
-        const html = mustache.render(content, data)
-        res.send(html)
+        const html = mustache.render(content, data);
+        res.send(html);
     } catch (error) {
-        console.log(error)
-        res.status(500).send(error)
+        console.log(error);
+        res.status(500).send(error);
     }
 })
 
 
 app.post('/content', async (req, res) => {
-    const url = req.body.url
-    var raw = req.body.raw
-    const js = req.body.js
+    const url = req.body.url;
+    var raw = req.body.raw;
+    const js = req.body.js;
     try {
         if (url) {
-            console.log('open: ' + url)
-            const {html, status} = await ssr(url, js)
+            console.log('open: ' + url);
+            const {html, status, final_url} = await ssr(url, js);
+            res.set("final_url", final_url);
             if (raw) {
-                res.status(status).send(html)
+                res.status(status).send(html);
             } else {
-                res.status(status).json(html)
+                res.status(status).json(html);
             }
         } else {
-            res.status(400).send("missing input value")
+            res.status(400).send("missing input value");
         }
     } catch (error) {
-        console.error(error)
-        res.status(500).send(error)
+        console.error(error);
+        res.status(500).send(error);
     }
 })
 
 app.post('/screenshot', async (req, res) => {
-    const url = req.body.url
-    const js = req.body.js
-    let ext
+    const url = req.body.url;
+    const js = req.body.js;
+    let ext;
     if (req.body.ext) {
-        ext = req.body.ext
+        ext = req.body.ext;
     } else {
-        ext = 'png'
+        ext = 'png';
     }
     const date = Date.now()
     let screenshotData = {
-        path: screenshotDir + date + '.' + ext
+        path: screenshotDir + date + '.' + ext,
     }
     try {
         if (url) {
-            console.log('open for screenshot: ' + url)
+            console.log('open for screenshot: ' + url);
             const context = await browser.createIncognitoBrowserContext();
-            const { page } = await loadPage(context, url, js)
-            await page.screenshot(screenshotData)
-            page.close()
-            context.close()
-            const filename = screenshotData.path
-            res.sendFile(filename)
+            const { page } = await loadPage(context, url, js);
+            await page.screenshot(screenshotData);
+            page.close();
+            context.close();
+            const filename = screenshotData.path;
+            res.sendFile(filename);
             res.on('finish', function() {
-                removeFile(filename)
+                removeFile(filename);
             });
         } else {
-            res.status(400).send("missing input value")
+            res.status(400).send("missing input value");
         }
     } catch (error) {
-        console.error(error)
-        res.status(500).send(error)
+        console.error(error);
+        res.status(500).send(error);
     }  
 })
 
 app.post('/pdf', async (req, res) => {
-    const url = req.body.url
-    const js = req.body.js
-    const date = Date.now()
-    var landscape = req.body.landscape
+    const url = req.body.url;
+    const js = req.body.js;
+    const date = Date.now();
+    var landscape = req.body.landscape;
     
     let pdfData = {
         path: pdfDir + date + '.pdf',
@@ -146,23 +147,23 @@ app.post('/pdf', async (req, res) => {
     }
     try {
         if (url) {
-            console.log('open for screenshot: ' + url)
+            console.log('open for screenshot: ' + url);
             const context = await browser.createIncognitoBrowserContext();
-            const { page } = await loadPage(context, url, js)
-            await page.pdf(pdfData)
-            page.close()
-            context.close()
-            const filename = pdfData.path
-            res.sendFile(filename)
+            const { page } = await loadPage(context, url, js);
+            await page.pdf(pdfData);
+            page.close();
+            context.close();
+            const filename = pdfData.path;
+            res.sendFile(filename);
             res.on('finish', function() {
-                removeFile(filename)
+                removeFile(filename);
             });
         } else {
-            res.status(400).send("missing input value")
+            res.status(400).send("missing input value");
         }
     } catch (error) {
-        console.error(error)
-        res.status(500).send(error)
+        console.error(error);
+        res.status(500).send(error);
     } 
 })
 
@@ -181,23 +182,29 @@ async function loadPage(context, url, js = false) {
 //        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 //        'Accept-Language': 'en-US,en;q=0.5'
 //    });
+    final_url = ""
     if (js) {
         page.setJavaScriptEnabled(true)
         const resp = await page.goto(url, { waitUntil: 'networkidle0' });
         status = resp.status();
+        final_url = resp._url
     } else {
         const resp = await page.goto(url);
         status = resp.status();
+        final_url = resp._url
     }
     console.log(await page.title())
-    return {page:page, status:status}
+    console.log(status)
+    console.log(url)
+    console.log(final_url)
+    return {page:page, status:status, final_url}
 }
 
 async function ssr(url, js = false) {
-    const { page, status } = await loadPage(browser, url, js)
+    const { page, status, final_url } = await loadPage(browser, url, js)
     const html = await page.content(); // serialized HTML of page DOM.
     page.close()
-    return {html:html, status: status};
+    return {html:html, status: status, final_url: final_url};
 }
 
 
